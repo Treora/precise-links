01ce08cbf77985ebe3d6b82686738dde413f091c
Gerben <gerben@treora.com>
2017-05-07 20:18:46 -0700

    Scroll to dereferenced segment.

--- src/dereference.js
+++ src/dereference.js
@@ -1,5 +1,6 @@
 import { selectorToRange } from 'dom-anchor-selector'
 import { uriToSelector } from 'selector-state-frags'
+import scrollIntoView from 'scroll-into-view'
 
 window.onload = processWindowLocation
 window.onhashchange = processWindowLocation
@@ -9,6 +10,10 @@ function processWindowLocation() {
     const selector = uriToSelector(uri)
     if (selector) {
         const range = selectorToRange(selector.selector)
+
+        const element = range.startContainer.parentElement
+        scrollIntoView(element, {time: 200})
+
         window.getSelection().addRange(range)
     }
 }
